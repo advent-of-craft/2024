c1d5e8e0aabc8de036f23cd46be312fbc29f2e33
Jean <jean.bouvattier@univ-grenoble-alpes.fr>
2024-12-05 14:18:54 +0100

    test: add stricter check on eid generate format

--- exercise/python/day05/tests/test_eid.py
+++ exercise/python/day05/tests/test_eid.py
@@ -6,28 +6,34 @@ from eid import Eid, ElvenSex
 
 
 class EIDTest(unittest.TestCase):
+
+    def generate_and_check_eid(self, *args, **kwargs):
+        eid = Eid.generate(*args, **kwargs)
+        assert_that(eid).is_length(8)
+        return eid
+
     def test_eid(self):
-        eid = Eid.generate(sex=ElvenSex.SLOUBI, birth_year=0, sn=1)
+        eid = self.generate_and_check_eid(sex=ElvenSex.SLOUBI, birth_year=0, sn=1)
         assert_that(eid).is_equal_to("10000195")
 
     def test_eid_sloubi(self):
-        eid = Eid.generate(ElvenSex.SLOUBI, birth_year=0, sn=1)
+        eid = self.generate_and_check_eid(ElvenSex.SLOUBI, birth_year=0, sn=1)
         assert_that(eid).starts_with("1")
 
     def test_eid_gagna(self):
-        eid = Eid.generate(ElvenSex.GAGNA, birth_year=0, sn=1)
+        eid = self.generate_and_check_eid(ElvenSex.GAGNA, birth_year=0, sn=1)
         assert_that(eid).starts_with("2")
 
     def test_eid_catact(self):
-        eid = Eid.generate(ElvenSex.CATACT, birth_year=0, sn=1)
+        eid = self.generate_and_check_eid(ElvenSex.CATACT, birth_year=0, sn=1)
         assert_that(eid).starts_with("3")
 
     def test_eid_birthyear(self):
-        eid = Eid.generate(ElvenSex.SLOUBI, birth_year=1950, sn=1)
+        eid = self.generate_and_check_eid(ElvenSex.SLOUBI, birth_year=1950, sn=1)
         assert_that(eid[1:3]).is_equal_to("50")
 
     def test_eid_birthyear_has_leading_zero(self):
-        eid = Eid.generate(ElvenSex.SLOUBI, birth_year=1, sn=1)
+        eid = self.generate_and_check_eid(ElvenSex.SLOUBI, birth_year=1, sn=1)
         assert_that(eid[1:3]).is_equal_to("01")
 
 
