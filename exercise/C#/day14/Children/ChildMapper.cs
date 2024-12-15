c3eb0694fe8d4036af44db599e1b0e4b18c26133
cedric <cedric.meleard@gmail.com>
2024-12-15 10:16:52 +0100

    Update ChildMapper to map birth date using date-only format
    Modified the mapping of the birth date to ensure only the date part is used. This improves data consistency and avoids potential time-related issues.

--- exercise/C#/day14/Children/ChildMapper.cs
+++ exercise/C#/day14/Children/ChildMapper.cs
@@ -12,22 +12,21 @@ public partial class ChildMapper
     [MapProperty(nameof(X5T78.N_2), nameof(Child.MiddleName))]
     [MapProperty(nameof(X5T78.N_3), nameof(Child.LastName))]
     [MapProperty(nameof(X5T78.CityOfBirth__pc), nameof(Child.BirthCity))]
-    [MapProperty(nameof(X5T78.Person_BD), nameof(Child.BirthDate))]
+    [MapProperty(nameof(X5T78.Person_BD), nameof(Child.BirthDate), Use = "MapDateOnly")]
     [MapProperty(nameof(X5T78.Salutation), nameof(Child.Gender))]
     [MapProperty(nameof(X5T78.ST_Num), $"{nameof(Child.Address)}.{nameof(Address.Number)}")]
     [MapProperty(nameof(X5T78.ST____Name), $"{nameof(Child.Address)}.{nameof(Address.Street)}")]
     [MapProperty(nameof(X5T78.ST_C), $"{nameof(Child.Address)}.{nameof(Address.City)}")]
     [MapProperty(nameof(X5T78.ST_CID), $"{nameof(Child.Address)}.{nameof(Address.CountryId)}")]
     public partial Child ToDto(X5T78 child);
-        
+
     private static DateOnly MapDateOnly(string birthDate)
     {
-        if( DateOnly.TryParseExact(birthDate, 
-               "dd/MM/yyyy", 
-               CultureInfo.InvariantCulture, 
-               DateTimeStyles.None, 
-               out var dateOfBirth))
-        {
+        if (DateOnly.TryParseExact(birthDate,
+                "dd/MM/yyyy",
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out var dateOfBirth)) {
             return dateOfBirth;
         }
         throw new ArgumentException("Date of birth is required");
