a3a7a76dbd42c11a7b45197c18746fa51dfe8911
Yoan Thirion <yoan.thirion@gmail.com>
2024-12-15 09:02:46 +0100

    feat: add solution for day 14 🎅

---
+++ solution/day14/Children/DTOs/Child.cs
@@ -0,0 +1,29 @@
+namespace Children.DTOs
+{
+    public class Child
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? NickName { get; set; }
+        public string? BirthCity { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public Address? Address { get; set; }
+    }
+
+    public enum Gender
+    {
+        Girl,
+        Boy
+    }
+
+    public class Address
+    {
+        public string Number { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int CountryId { get; set; }
+    }
+}
