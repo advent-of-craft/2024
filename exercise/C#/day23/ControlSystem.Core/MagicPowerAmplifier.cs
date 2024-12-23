bee6bbf4d89609f328f9c0f9cc4468f8ec02df2a
cedric <cedric.meleard@gmail.com>
2024-12-23 21:57:37 +0100

    refactor(core): simplify MagicPowerAmplifier by removing redundant field

--- exercise/C#/day23/ControlSystem.Core/MagicPowerAmplifier.cs
+++ exercise/C#/day23/ControlSystem.Core/MagicPowerAmplifier.cs
@@ -1,15 +1,11 @@
 namespace ControlSystem.Core;
 
-public class MagicPowerAmplifier
+public class MagicPowerAmplifier(AmplifierType amplifierType)
 {
-    private readonly AmplifierType _amplifierType;
-
-    public MagicPowerAmplifier(AmplifierType amplifierType) => _amplifierType = amplifierType;
-
     public float Amplify(float magicPower)
     {
         if (magicPower > 0)
-            return magicPower * _amplifierType.GetMultiplier();
+            return magicPower * amplifierType.GetMultiplier();
         return magicPower;
     }
 }
